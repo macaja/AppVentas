cb0df2bc0d7deb43a6fb9d3ab0dafb388a15adb3
Mauricio <cardonajaramillomauricio@gmail.com>
2016-09-14 23:14:42 -0500

    Inicio de Proyecto

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appVentasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appVentasApp
+ */
+angular.module('appVentasApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
